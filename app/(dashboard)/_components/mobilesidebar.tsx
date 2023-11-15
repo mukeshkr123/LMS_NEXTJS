98f222c81b3f22563e2083d9f73f0908334e6f70
mukesh kumar <mkmehta2041@gmail.com>
2023-11-15 12:23:30 +0530

    add mobile navbar

--- app/(dashboard)/_components/mobilesidebar.tsx
+++ app/(dashboard)/_components/mobilesidebar.tsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import Sidebar from "./sidebar";
 
 export const Mobilesidebar = () => {
-  return <Menu />;
+  return (
+    <Sheet>
+      <SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+        <Menu />
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0 bg-white">
+        <Sidebar />
+      </SheetContent>
+    </Sheet>
+  );
 };
--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Mobilesidebar } from "./mobilesidebar";
+import NavbarRoutes from "@/components/navbar-routes";
 
 const Navbar = () => {
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm  ">
       <Mobilesidebar />
+      <NavbarRoutes />
     </div>
   );
 };
